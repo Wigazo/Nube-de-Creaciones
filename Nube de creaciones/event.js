0d2368b2e3e2cf8bf398a85f06ced40fdc422897
Wigazo <wigazo@hotmail.com>
2015-08-13 18:06:54 -0300

    2.2.3
    http://www.sporepedia2.com/t13436p400-proyecto-nube-de-creaciones-fase-de-recuperacion#311234

--- Nube de creaciones/event.js
+++ Nube de creaciones/event.js
@@ -20,7 +20,7 @@
 			};
 			xhr.send();
 		case "abrirDB":
-			chrome.tabs.query({url: "https://www.dropbox.com/home/BACKUP/" + datos.subforo + "/" + datos.tid}, function(tabs){
+			chrome.tabs.query({url: "https://www.dropbox.com/home/BACKUP/" + datos.subforo.replace("í","%C3%AD").replace(" ","%20") + "/" + datos.tid}, function(tabs){
 				if(tabs.length == 0){
 					chrome.tabs.query({active: true}, function(dbtabs){
 						var client = new Dropbox.Client({ key: "b2r6yecvzwp382k", token: "lNg1OCSRRB0AAAAAAAAILmGCKpHT2k21aVHb4eHatND1JPRR9cHIALN5DuQDANIf"});
@@ -29,8 +29,8 @@
 								console.log(error);
 							}
 							chrome.tabs.create({url: "https://www.dropbox.com/home/BACKUP/" + datos.subforo + "/" + datos.tid, active: false, index: dbtabs[0].index+1}, function(tab){
-								chrome.tabs.query({url: "https://www.dropbox.com/home/BACKUP/" + datos.subforo + "/" + datos.tid}, function(tabs){
-									if(tabs.length != 1){
+								chrome.tabs.query({url: "https://www.dropbox.com/home/BACKUP/" + datos.subforo.replace("í","%C3%AD").replace(" ","%20") + "/" + datos.tid}, function(tabs){
+									if(tabs.length > 1){
 										chrome.tabs.remove(tab.id);
 									}
 								});
